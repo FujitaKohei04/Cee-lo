fb426996eff60430491f00d2453b647c6c184fd4
fujita_kohei <fujita.kohei04@gmail.com>
2026-04-01 01:05:03 +0900

    feat: help modal

--- app/components/CeeLo/CeeLo.tsx
+++ app/components/CeeLo/CeeLo.tsx
@@ -3,17 +3,26 @@
 import { useState } from "react";
 import styles from "./CeeLo.module.css";
 import { Die } from "@/app/components/Dice/Dice";
+import { HelpModal } from "../HelpModal/HelpModal";
+import Modal from "react-modal";
 
 export function CeeLo() {  
+    Modal.setAppElement("body");
     const [dice, setDie] = useState<number[]>([1, 1, 1]);
+    const [isOpenHelpModal, setIsOpenHelpModal] = useState(false);
 
     const rollDice = () => {
         setDie([Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1]);
     }
 
     return (
-    <div className={styles.ceeLoContainer}>
-        
+    <div 
+        className={styles.ceeLoContainer}
+    >
+        <button 
+            className={`${styles.button} ${styles.helpButton}`}
+            onClick={() => setIsOpenHelpModal(true)}
+        >?</button>
         <div className={styles.diceContainer}>
             <div className={styles.die}><Die pipCount={dice[0]} /></div>
             <div className={styles.die}><Die pipCount={dice[1]} /></div>
@@ -22,8 +31,9 @@ export function CeeLo() {
         <button 
             onClick={() => rollDice()}
         >
-            <div className={styles.rollButton}>roll</div>
+            <div className={styles.button}>roll</div>
         </button>
+        <HelpModal isOpen={isOpenHelpModal} closeModal={() => setIsOpenHelpModal(false)} />
     </div>
     );
 }
---
+++ app/components/HelpModal/HelpModal.tsx
@@ -0,0 +1,40 @@
+import Modal from "react-modal";
+import styles from "./HelpModal.module.css";
+import { Die } from "../Dice/Dice";
+
+export const HelpModal = ({ isOpen, closeModal }: {isOpen: boolean, closeModal: () => void}) => {
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={closeModal}
+            portalClassName="ceeLoContainer"
+        >
+            <button onClick={closeModal}>x</button>
+            <div className={styles.descContainer}>
+                <Comb pipCounts={[1, 1, 1]} name="Triples" desc="x3"/>
+                <Comb pipCounts={[4, 5, 6]} name="High Straight" desc="x2"/>
+                <Comb pipCounts={[1, 2, 3]} name="Low Straight" desc="x-2"/>
+                <Comb pipCounts={[2, 2, 4]} name="Point" desc="x1"/>
+                <Comb pipCounts={[1, 2, 4]} name="No Combination" desc="x1"/>
+            </div>
+        </Modal>
+    )
+}
+
+const Comb = ({ pipCounts, name, desc }: {pipCounts: number[], name: string, desc: string}) => {
+    return (
+        <div className={styles.combContainer}>
+            <div className={styles.die}>
+                <Die pipCount={pipCounts[0]}></Die>
+            </div>
+            <div className={styles.die}>
+                <Die pipCount={pipCounts[1]}></Die>
+            </div>
+            <div className={styles.die}>
+                <Die pipCount={pipCounts[2]}></Die>
+            </div>
+            <div className={styles.name}>{name}</div>
+            <div className={styles.desc}>{desc}</div>
+        </div>
+    )
+}
